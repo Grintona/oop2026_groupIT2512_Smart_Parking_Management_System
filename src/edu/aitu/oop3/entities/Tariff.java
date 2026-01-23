a47865f9801355021cfd2cbca0a574641cde2ffa
Ass1013 <asylbulembekov@gmail.com>
2026-01-23 10:38:10 +0500

    Assyl Bulembekov created tariff entitie

--- src/edu/aitu/oop3/entities/Tariff.java
+++ src/edu/aitu/oop3/entities/Tariff.java
@@ -1,4 +1,34 @@
 package edu.aitu.oop3.entities;
 
+
 public class Tariff {
+
+    private int id;
+    private String name;
+    private int pricePerHour;
+
+    public Tariff(int id, String name, int pricePerHour) {
+        this.id = id;
+        this.name = name;
+        this.pricePerHour = pricePerHour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(int pricePerHour) {
+        if (pricePerHour < 0) {
+            throw new IllegalArgumentException("Price per hour cannot be negative");
+        }
+        this.pricePerHour = pricePerHour;
+    }
 }
