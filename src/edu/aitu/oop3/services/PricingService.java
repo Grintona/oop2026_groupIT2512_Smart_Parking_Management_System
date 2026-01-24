9ac16f3c8aa0c1ad71b91760a8b1b225c396ee42
Ass1013 <asylbulembekov@gmail.com>
2026-01-24 15:37:34 +0500

    Assyl Bulembekov created TariffRepository in repositories and PricingService in services

--- src/edu/aitu/oop3/services/PricingService.java
+++ src/edu/aitu/oop3/services/PricingService.java
@@ -1,4 +1,27 @@
 package edu.aitu.oop3.services;
 
+import edu.aitu.oop3.entities.Reservation;
+import edu.aitu.oop3.entities.Tariff;
+import edu.aitu.oop3.repositories.TariffRepository;
+
 public class PricingService {
+
+    private final TariffRepository tariffRepository;
+
+    public PricingService(TariffRepository tariffRepository) {
+        this.tariffRepository = tariffRepository;
+    }
+
+    public int calculatePrice(Reservation reservation, int hours) {
+        if (hours <= 0) {
+            throw new IllegalArgumentException("Hours must be positive");
+        }
+
+        Tariff tariff = tariffRepository.findById(reservation.getTariffId());
+        if (tariff == null) {
+            throw new RuntimeException("Tariff not found: " + reservation.getTariffId());
+        }
+
+        return tariff.getPricePerHour() * hours;
+    }
 }
