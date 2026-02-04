f270fec29f70484e2f7290046c60814c49cf1d72
Ass1013 <asylbulembekov@gmail.com>
2026-02-04 22:25:54 +0500

    Assyl Bulembekov created TarrifConfig in config

--- src/edu/aitu/oop3/config/TariffConfig.java
+++ src/edu/aitu/oop3/config/TariffConfig.java
@@ -1,4 +1,22 @@
 package edu.aitu.oop3.config;
 
 public class TariffConfig {
+
+    private static final TariffConfig INSTANCE = new TariffConfig();
+
+    private int defaultTariffId = 1;
+
+    private TariffConfig() { }
+
+    public static TariffConfig getInstance() {
+        return INSTANCE;
+    }
+
+    public int getDefaultTariffId() {
+        return defaultTariffId;
+    }
+
+    public void setDefaultTariffId(int defaultTariffId) {
+        this.defaultTariffId = defaultTariffId;
+    }
 }
