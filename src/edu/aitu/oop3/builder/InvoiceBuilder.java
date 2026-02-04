deeb26881423c616bedc0e600b117df2d08b1c5d
Ass1013 <asylbulembekov@gmail.com>
2026-02-04 19:44:39 +0500

    Assyl Bulembekov created InvoiceBuilder in builder and Invoice in entities

--- src/edu/aitu/oop3/builder/InvoiceBuilder.java
+++ src/edu/aitu/oop3/builder/InvoiceBuilder.java
@@ -1,4 +1,41 @@
 package edu.aitu.oop3.builder;
 
+import edu.aitu.oop3.entities.Invoice;
+
 public class InvoiceBuilder {
+
+    private int reservationId;
+    private long hours;
+    private int cost;
+    private String plate;
+    private String tariffName;
+
+    public InvoiceBuilder reservationId(int reservationId) {
+        this.reservationId = reservationId;
+        return this;
+    }
+
+    public InvoiceBuilder hours(long hours) {
+        this.hours = hours;
+        return this;
+    }
+
+    public InvoiceBuilder cost(int cost) {
+        this.cost = cost;
+        return this;
+    }
+
+    public InvoiceBuilder plate(String plate) {
+        this.plate = plate;
+        return this;
+    }
+
+    public InvoiceBuilder tariffName(String tariffName) {
+        this.tariffName = tariffName;
+        return this;
+    }
+
+    public Invoice build() {
+        return new Invoice(reservationId, hours, cost, plate, tariffName);
+    }
 }
--- src/edu/aitu/oop3/entities/Invoice.java
+++ src/edu/aitu/oop3/entities/Invoice.java
@@ -1,4 +1,48 @@
 package edu.aitu.oop3.entities;
 
 public class Invoice {
+    private final int reservationId;
+    private final long hours;
+    private final int cost;
+    private final String plate;
+    private final String tariffName;
+
+    public Invoice(int reservationId, long hours, int cost, String plate, String tariffName) {
+        this.reservationId = reservationId;
+        this.hours = hours;
+        this.cost = cost;
+        this.plate = plate;
+        this.tariffName = tariffName;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public long getHours() {
+        return hours;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public String getTariffName() {
+        return tariffName;
+    }
+
+    @Override
+    public String toString() {
+        return "========== INVOICE ==========\n"
+                + "Reservation ID : " + reservationId + "\n"
+                + "Plate          : " + plate + "\n"
+                + "Tariff         : " + tariffName + "\n"
+                + "Hours          : " + hours + "\n"
+                + "Total cost     : " + cost + "\n"
+                + "=============================";
+    }
 }
