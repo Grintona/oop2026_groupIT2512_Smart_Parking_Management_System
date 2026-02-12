b895b5b76faa5e55a95ce986864a0abd6df70369
Ass1013 <asylbulembekov@gmail.com>
2026-02-12 18:29:44 +0500

    Assyl Bulembekov created monitoringcomponent,paymentcomponent,reportingcomponent in components

--- src/edu/aitu/oop3/components/MonitoringComponent.java
+++ src/edu/aitu/oop3/components/MonitoringComponent.java
@@ -1,4 +1,16 @@
 package edu.aitu.oop3.components;
 
+import edu.aitu.oop3.common.ListResult;
+import edu.aitu.oop3.entities.ParkingSpot;
+import edu.aitu.oop3.entities.Reservation;
+
 public class MonitoringComponent {
+
+    public int countFreeSpots(ListResult<ParkingSpot> freeSpots) {
+        return freeSpots.getTotalCount();
+    }
+
+    public int countActiveReservations(ListResult<Reservation> reservations) {
+        return reservations.getTotalCount();
+    }
 }
